6c002e7e497825369c3b36ca042f71d201de5d2a
armstrong_codes <a.hirwa2@alustudent.com>
2025-03-06 00:03:11 -0800

    Updated

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -5,7 +5,10 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end="")
             num += 1
-        except(TypeError, ValueError):
+        except (ValueError, TypeError):
             continue
+        except IndexError:
+            break  # Stop the loop if we try to access an out-of-range index
     print()
-    return numt
+    return num
+
